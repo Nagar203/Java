d99a5520f01609870fb19a809bee170677203069
Chandra Mohan Nagar <36680507+Nagar203@users.noreply.github.com>
2025-09-20 23:57:46 +0530

    Update Example.java

--- L-20/FunctionInherit/Example.java
+++ L-20/FunctionInherit/Example.java
@@ -3,11 +3,13 @@ class A{
 		System.out.print("Class A");
 	}
 }
+
 class B extends A{
 	static void f1(){
 		System.out.print("Class B");
 	}
 }
+
 public class Example{
 	public static void main(String []args){
 		B obj=new B();
